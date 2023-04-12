b373a4330bcfb823991fce3e3aae8313dcb2f20a
franciscoalfar@gmail.com <*Fran2811*>
2023-04-11 20:50:59 -0400

    config

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,4 +1,4 @@
 export const Global =  {
-    url:"http://ec2-3-131-142-73.us-east-2.compute.amazonaws.com:3000/api/"
+    url:"http://ec2-18-219-218-242.us-east-2.compute.amazonaws.com:3000/api/"
 
 }
